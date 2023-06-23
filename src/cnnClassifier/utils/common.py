de2d3ca9b2f174789d1d0dc05be5f75acd8bfd7f
maaz-shahid99 <goluthakle.com>
2023-06-23 17:25:04 +0530

    Functions Edited

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -10,10 +10,12 @@ from pathlib import Path
 from typing import Any
 import base64
 
+
+
 @ensure_annotations
-def read_yaml(path_to_yaml:Path) -> ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
-    
+
     Args:
         path_to_yaml (str): path like input
 
@@ -22,35 +24,38 @@ def read_yaml(path_to_yaml:Path) -> ConfigBox:
         e: empty file
 
     Returns:
-        Configbox: ConfigBox type
+        ConfigBox: ConfigBox type
     """
-
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
     
+
+
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
+
     Args:
         path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs to be created. Default to false.
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
 
+
 @ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
-    
+
     Args:
         path (Path): path to json file
         data (dict): data to be saved in json file
@@ -59,6 +64,8 @@ def save_json(path: Path, data: dict):
         json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
+
+
 
 
 @ensure_annotations
